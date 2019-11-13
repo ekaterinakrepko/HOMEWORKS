e9cabbf69d527639571e86ad5f850bda404be305
ekaterinakrepko <ekaterina.krepko@gmail.com>
2019-11-13 23:12:03 +0200

    1111

---
+++ homework11/script.js
@@ -0,0 +1,48 @@
+// 1
+let min;
+let sec;
+let hours;
+min = 60;
+sec = 60;
+hours = 3;
+console.log((min  * sec) * hours);
+
+//2
+
+var num = 1;
+num += 12;
+console.log(num);
+num-=14;
+console.log(num);
+num*=5;
+console.log(num);
+num/=7;
+console.log(num);
+num+=1;
+console.log(num);
+num-=1;
+console.log(num);
+
+//3
+let i  = 1;
+let f = 2;
+let g = 5;
+console.log(i + f + g);
+
+//4
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(a -b);
+console.log(c + d);
+console.log(result);
+
+//5
+
+let name;
+name = "Katya";
+/*let phrase = `$ {name} `;
+console.log(phrase);*/
+console.log("Привет " + name + "!")
